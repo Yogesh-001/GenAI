382b48d330878cfaea4aa35802b4a434438d956c
Yogesh-001 <74003370+Yogesh-001@users.noreply.github.com>
2025-01-24 14:23:08 +0530

    URLAgentBot V.0.0.1

--- AskURLs/URLAgentBot.py
+++ AskURLs/URLAgentBot.py
@@ -1,23 +1,32 @@
 from langchain_community.vectorstores import FAISS
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain.prompts import PromptTemplate
-from langchain.chains import LLMChain
-from langchain.agents import initialize_agent
-from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
+from langchain.chains import RetrievalQA
+from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline, BitsAndBytesConfig
 from langchain_huggingface import HuggingFacePipeline
-from langchain.chains import LLMChain
 from functions import getHTMLdata, get_text_chunks, get_vector_store
-import streamlit as st
 import os
+from dotenv import load_dotenv
 
-def get_conversational_chain():
+
+load_dotenv()
+def get_chat_chain():
     model_name = "meta-llama/Llama-2-7b-chat-hf"
-    access_token = os.getenv("HF-ACCESS-TOKEN")
+    access_token = os.getenv("HF_ACCESS_TOKEN")
+    quantization_config = BitsAndBytesConfig(
+    load_in_4bit=True, 
+    bnb_4bit_compute_dtype="float16",
+    bnb_4bit_use_double_quant=True,
+    llm_int8_threshold=6.0,
+    llm_int8_has_fp16_weight=False
+    )
     tokenizer = AutoTokenizer.from_pretrained(model_name, token=access_token)
-    model = AutoModelForCausalLM.from_pretrained(model_name, token=access_token, device_map="auto")
-    hf_pipeline = pipeline("text-generation", model=model, tokenizer=tokenizer, max_new_tokens=500)
-    chat_pipeline = HuggingFacePipeline(pipeline=hf_pipeline)
-  
+    model = AutoModelForCausalLM.from_pretrained(model_name, token=access_token,quantization_config = quantization_config, device_map="auto")
+    hf_pipeline = pipeline("text-generation", model=model, tokenizer=tokenizer, max_new_tokens=100)
+    llm = HuggingFacePipeline(pipeline=hf_pipeline)
+
+    vectorstore = VectorStore()
+
     prompt_template = """
     You are a helpful assistant. Your role is to answer the user's question based on the provided context.
 
@@ -25,33 +34,32 @@ def get_conversational_chain():
     - If the answer is not in the context, respond with:
     'The answer is not available in the context.'
     - Avoid providing incorrect or speculative answers.
-
+    
     Context:
     {context}
 
     Question:
-    {input}
+    {question}
     """
     prompt = PromptTemplate(
-        input_variables=["context", "input"],
+        input_variables=["context", "question"],
         template=prompt_template,
     )
 
-    llm_chain = LLMChain(
-        llm=chat_pipeline,
-        prompt=prompt,
-        verbose=True,
+    chat_chain = RetrievalQA.from_chain_type(
+        llm = llm,
+        retriever = vectorstore.as_retriever(),
+        return_source_documents = True,
+        chain_type_kwargs={"prompt" : prompt}
     )
 
-    return llm_chain
+    return chat_chain
 
-def user_input(user_question):
+
+def VectorStore():
     embedding_model = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
-    new_db = FAISS.load_local("faiss_index", embedding_model, allow_dangerous_deserialization=True)
-    docs = new_db.similarity_search(user_question)
-    result = " ".join([doc.page_content for doc in docs])
-    print("Tool Output:", result)
-    return result
+    vectorstore = FAISS.load_local("faiss_index", embedding_model, allow_dangerous_deserialization=True)
+    return vectorstore
 
 def conversation():
     print("Chat with URL Content \n")
@@ -61,21 +69,24 @@ def conversation():
         text_chunks = get_text_chunks(raw_text.get_text(separator="\n", strip=True))
         get_vector_store(text_chunks)
 
-    conversation_chain = get_conversational_chain()
+    conversation_chain = get_chat_chain()
 
     while True:
-        user_question = input("Ask a question based on the URL content (or type 'exit' to quit): \n")
-        if user_question.lower() == 'exit':
+        question = input("Ask a question based on the URL content (or type 'exit' to quit): \n")
+        if question.lower() == 'exit':
             print("Exiting the chat. Goodbye!")
             break
-          
-        if user_question.strip():
-            print("\nSearching for relevant context...\n")
-            context = user_input(user_question)
-            print("\nGenerating an answer......\n")
 
-            response = conversation_chain.run(input=user_question,context=context)
-            print(f"Agent: {response}")
+        if question.strip():
+            print("\nGenerating an answer......\n")
+            response = conversation_chain.invoke({"query" : question})
+            answer = response["result"]
+            sources = response["source_documents"]
+
+            print(f"Agent : {answer}")
+            print("\nSources:")
+            for source in sources:
+                print(f"-  {source.metadata.get('source', 'unknown')} :  {source.page_content[:200]}...")
 
 if __name__ == "__main__":
     conversation()
